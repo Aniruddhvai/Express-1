ff9eec3e2732c4eab39a7cff75574f4ed837ece9
Aniruddhvai <aniruddhvaishnav786@gmail.com>
2024-04-04 16:37:02 +0530

    body parser

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 
 const express = require('express')
 
+const bodyParser =  require ('body-parser');
+
 const app =  express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+///this body parser use only for form body
+// body parser - read data and put  in to the body
 
 // adding middleware
 // req, and res in object and next is a function
-app.use((req , res, next )=>{
-
-    console.log('in the middleware - 1');
-    next();//  this allow the request to continue to the next middleware in line 
-});
-
-app.use((req , res, next )=>{
-
-    console.log('in the middleware - 2')
-
-    // sending response from here
-
-    res.send( { name : "aman"})
+app.use('/add-product', (req , res, next )=>{
     
+    //the submiited data will be sent to /product route
+    res.send('<form action = "/product" method= "POST"><input type " text" name ="tittle"><input type="number"  name="productSize" ><button type ="submit">Add Product</button></form>')
 });
 
 
+app.use('/product', (req , res, next )=>{
+
+    console.log(req.body);// extract data what the user send me 
+    
+    res.redirect('/');
+});
+
+app.use('/', (req , res, next )=>{
+
+    console.log("Hello");// extract data what the user send me 
+    res.send ("hello express")
+
+});
 app.listen(4100);
 
 
